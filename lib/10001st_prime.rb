89898bab0d727d190659e242401df8ea686de8ba
nickzaf95 <nicholaszafiriou@gmail.com>
2020-10-08 13:39:49 +0300

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,40 @@
 # Implement your procedural solution here!
+# const isPrime = num => {
+#     for(let i = 2; i < num; i++)
+#       if(num % i === 0) return false;
+#     return num > 1;
+# }
+
+# const problemSeven = () => {
+#     let array = []
+#     let i = 2
+#     while (array.length !== 10001) {
+#         if (isPrime(i)) {
+#             array.push(i)
+#         }
+#         i = i + 1
+#     }
+#     return array[10000]
+# }
+
+def isPrime(num)
+    for i in 2...num 
+        if num % i == 0
+            return false
+        end
+    end
+    return true
+end
+
+
+def prime_number_for(n)
+    array = []
+    i = 2
+    while array.length != n do
+        if isPrime(i)
+            array << i 
+        end
+        i = i + 1
+    end
+    return array[n-1]
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class Prime
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def isPrime(num)
+        for i in 2...num 
+            if num % i == 0
+                return false
+            end
+        end
+        return true
+    end
+    
+    
+    def number()
+        array = []
+        i = 2
+        while array.length != @limit do
+            if isPrime(i)
+                array << i 
+            end
+            i = i + 1
+        end
+        return array[@limit - 1]
+    end
+
+end
